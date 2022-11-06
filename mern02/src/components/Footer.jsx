fade5976add218c0636860cf66d5fb1bddc27b21
SciGuyTy <10765359+SciGuyTy@users.noreply.github.com>
2022-11-05 20:21:25 -0600

    Moved footer to seperate component

---
+++ mern02/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <hr />
+            <p>Repository link: <a href='https://github.com/SciGuyTy/csci331-mern' target={"_blank"}>https://github.com/SciGuyTy/csci331-mern</a></p>
+            <hr />
+        </div>
+    )
+}
+
+export default Footer;
